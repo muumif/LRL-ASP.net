c9e0cb603d52ed9e509f6d22be3eb86397ade6dd
muumi <chrisrobin.soon@gmail.com>
2023-01-31 19:38:11 +0200

    main

---
+++ First-website-LRL/First/Controllers/ListController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcMovie.Controllers;
+
+public class ListController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        
+        return View();
+    }
+}
